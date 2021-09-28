4e5a53bb6ec4cf499e709e60e51b0431aa80d15c
DefaultPerson <armyuser90151@gmail.com>
2021-09-28 16:25:24 +0300

    Написан скрипт для автоматической настройки

--- arch_linux_setup/install_script_2.sh
+++ arch_linux_setup/install_script_2.sh
@@ -1,32 +1,24 @@
 # Ставим защиту на логи ядра
-yay -S picom-jonaburg-git
-su
-touch /etc/sysctl.d/51-dmesg-restrict.conf
-echo 'kernel.dmesg_restrict = 1' >> /etc/sysctl.d/51-dmesg-restrict.conf
+sudo sh -c"touch /etc/sysctl.d/51-dmesg-restrict.conf"
+sudo sh -c "echo 'kernel.dmesg_restrict = 1' >> /etc/sysctl.d/51-dmesg-restrict.conf"
 
 # Ставим защиту от перебора пароля
-echo '
+sudo sh -c "echo '
 password required pam_pwquality.so retry=2 minlen=10 difok=6 dcredit=-1 ucredit=-1 ocredit=-1 lcredit=-1 [badwords=myservice mydomain] enforce_for_root
-password required pam_unix.so use_authtok sha512 shadow' >> /etc/pam.d/passwd
-echo 'auth optional pam_faildelay.so delay=4000000' >> /etc/pam.d/system-login
+password required pam_unix.so use_authtok sha512 shadow' >> /etc/pam.d/passwd"
+sudo sh -c "echo 'auth optional pam_faildelay.so delay=4000000' >> /etc/pam.d/system-login"
 
 # Настраиваем micro
 cd
-sudo rm .config/micro/settings.json
-sudo touch .config/micro/settings.json
-sudo echo '{
-	"autosave":true,
-	"scrollspeed":2,
-	"softwrap":true,
-}' >> .config/micro/settings.json
+sudo cp Linux_misc/settings.json .config/micro
 
 # Устанавливаем шрифты
-sudo rm /etc/vconsole.conf
-sudo touch /etc/vconsole.conf
-sudo echo '
+sudo sh -c "sudo rm /etc/vconsole.conf"
+sudo sh -c "touch /etc/vconsole.conf"
+sudo sh -c "echo '
 FONT=ter-v32
 KEYMAP=us
-' >> /etc/vconsole.conf
+' >> /etc/vconsole.conf"
 
 # Включаем переключение раскладки
 sudo localectl set-x11-keymap us,ru pc104 "" grp:alt_shift_toggle
@@ -48,4 +40,5 @@ chmod +x .config/polybar/polybar-scripts/polybar-speedtest.py
 cp -r Linux_misc/rofi .config
 
 # Установка picom
+yay -S picom-jonaburg-git
 cp Linux_misc/picom.conf .config
